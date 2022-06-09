276fe7f1cceac56300ae30f6e8797e9f4864178f
D-ENIKE <larahkeji@gmail.com>
2022-06-09 16:55:01 -0700

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,21 +2,15 @@
 /**
  * _isupper - checks for uppercase
  * @c: upper case character
- * Return: 1 if c is uppercase, 0 if otherwise
+ * Return: 1 if true, 0 if false
  */
 int _isupper(int c)
 
 
 {
-	if (c >= 'A' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
 
-	{
 		return (1);
 
-	}
-	else
-	{
-		return (0);
-	}
-	putchar('\n')
+	return (0);
 }
