6d8cee189b6d406a889aee20b8f69ec020383bca
D-ENIKE <larahkeji@gmail.com>
2022-06-16 15:15:36 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,11 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 
 	while
-(*(s1 + i) == *(s2 + i) && *(s1 + i)
+(*(s1 + i) == *(s2 + i) && *(s1 + i))
 		i++;
 
 	if (*(s2 + i))
 		return (*(s1 + i) - *(s2 + i));
-
-		else
-
-	return (0);
+	else
+		return (0);
 }
