9e923376a96c4d2b94ca5de23c93983be122b449
D-ENIKE <larahkeji@gmail.com>
2022-06-19 15:32:14 -0700

    done

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-nt _putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -17,6 +17,19 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void print_more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
