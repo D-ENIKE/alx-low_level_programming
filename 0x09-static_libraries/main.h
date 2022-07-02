e40c4797c2fdabab1abc352e84df47d07f8724f8
D-ENIKE <larahkeji@gmail.com>
2022-07-02 13:45:25 -0700

    done

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
 #ifndex MAIN_H
 #define MAN_H
+
 int _putchar(char c);
 int _slower(int c);
 int _isalpha(int c);
@@ -20,4 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
+
+#endi
